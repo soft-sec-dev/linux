0e23e9e3f509075432952fe5e3dbaa164f37085c
jordan-ubuntu <jordanelcracken@gmail.com>
2023-05-29 17:49:54 -0500

    feat: add update to Read variables

--- git-submit.sh
+++ git-submit.sh
@@ -1,7 +1,11 @@
 # !/bin/bash
-# basic script to upload changes to github
-clear  
+#
+#? basic script to upload changes to github
+clear 
+echo Add the name is the commit to upload changes
+read COMMIT
+
 git add .
-git commit -m "feat: add $1"
+git commit -m "feat: add $COMMIT"
 git push
 exit 0
---
+++ linux-fundamentals/module-2/readVariabels.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+#? Ask what do you want to kill
+echo which process do you want to kill
+read TOKILL
+kill $(ps aux | grep $TOKILL | grep -v grep | awk '{print $2}')
