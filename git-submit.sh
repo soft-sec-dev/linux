94c2ee56a4a50dbc0899613d29834c4192959053
jordan-ubuntu <jordanelcracken@gmail.com>
2023-05-30 09:38:43 -0500

    fix: update Read variables

--- git-submit.sh
+++ git-submit.sh
@@ -6,6 +6,6 @@ echo Add the name is the commit to upload changes
 read COMMIT
 
 git add .
-git commit -m "feat: add $COMMIT"
+git commit -m "$COMMIT"
 git push
 exit 0
--- linux-fundamentals/module-2/readVariabels.sh
+++ linux-fundamentals/module-2/readVariabels.sh
@@ -3,4 +3,4 @@
 #? Ask what do you want to kill
 echo which process do you want to kill
 read TOKILL
-kill $(ps aux | grep $TOKILL | grep -v grep | awk '{print $2}')
+kill  $(ps aux | grep $TOKILL | grep -v grep | awk '{ print $2 }')
